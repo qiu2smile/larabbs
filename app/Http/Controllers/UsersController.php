4f1bc6455720a39cc2ffeee040f7d7388f2dc6c3
qiu <w0212X>
2019-01-18 17:02:05 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,12 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     /**
      * 个人中心展示
      * @param User $user
@@ -27,10 +33,14 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $uploader, User $user){
+
+        $this->authorize('update', $user);
 
         $data = $request->all();
 
