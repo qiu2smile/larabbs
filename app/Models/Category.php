b901b9994e8f5d9c4e248663021574f04642f4f1
qiu <w0212X>
2019-01-19 19:17:50 +0800

    修复 N+1 问题

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,4 +11,5 @@ class Category extends Model
         'name', 'description',
     ];
 
+
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,26 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+
+    /**
+     * 话题分类
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(){
+
+        return $this->belongsTo(Category::class);
+
+    }
+
+    /**
+     * 用户表
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+
+        return $this->belongsTo(User::class);
+
+    }
+
 }
