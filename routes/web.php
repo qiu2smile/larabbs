71af902386243e9ffcbf8fa7e58ab29e6d1d6d69
qiu <w0212X>
2019-01-21 15:40:23 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -38,5 +38,8 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 
-
+//
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//话题分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
